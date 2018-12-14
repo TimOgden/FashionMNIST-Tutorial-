9b485f5bbf6b370d734faa222d218a31bd79f397
Tim Ogden <togden1999@gmail.com>
2018-12-14 18:20:05 -0500

    Added matplotlib functions to analyze input data

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -10,10 +10,26 @@ fashion_mnist = keras.datasets.fashion_mnist
 
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+class_names = ['T-shirt', 'Pants', 'Sweatshirt', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-plt.figure()
-plt.imshow(train_images[1])
-plt.colorbar()
-plt.grid(False)
+#Don't need this anymore
+#plt.figure()
+#plt.imshow(train_images[1])
+#plt.colorbar()
+#plt.grid(False)
+#plt.show()
+
+#Normalizing data
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+plt.figure(figsize=(10,10))
+for i in range(25, 50):
+	plt.subplot(5,5,i+1-25)
+	plt.xticks([])
+	plt.yticks([])
+	plt.grid(False)
+	plt.imshow(train_images[i], cmap=plt.cm.binary)
+	plt.xlabel(class_names[train_labels[i]])
 plt.show()
+

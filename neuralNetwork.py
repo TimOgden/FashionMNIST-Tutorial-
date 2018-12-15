7fc0ece793e41b09e994e0d69264f7ee57bcf1fb
Tim Ogden <togden1999@gmail.com>
2018-12-14 19:20:14 -0500

    Used pie graph instead of the bar that the tutorial recommended

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -23,13 +23,68 @@ class_names = ['T-shirt', 'Pants', 'Sweatshirt', 'Dress', 'Coat', 'Sandal', 'Shi
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
-plt.figure(figsize=(10,10))
-for i in range(25, 50):
-	plt.subplot(5,5,i+1-25)
+#plt.figure(figsize=(10,10))
+#for i in range(25, 50):
+#	plt.subplot(5,5,i+1-25)
+#	plt.xticks([])
+#	plt.yticks([])
+#	plt.grid(False)
+#	plt.imshow(train_images[i], cmap=plt.cm.binary)
+#	plt.xlabel(class_names[train_labels[i]])
+#plt.show()
+
+model = keras.Sequential([
+	keras.layers.Flatten(input_shape=(28,28)), #Flatten just takes the 28 by 28 dim array and turns it into a 28*28=784 dim array
+	keras.layers.Dense(128, activation=tf.nn.relu), #Not sure why they chose relu activation type or used 128 nodes
+	keras.layers.Dense(10, activation=tf.nn.softmax) #10 nodes because 10 possible outcomes and activation type softmax choses the highest value of all nodes in layer
+])
+#Takes an optimizer, loss function, and metrics to measure
+model.compile(optimizer=tf.train.AdamOptimizer(),
+				loss='sparse_categorical_crossentropy', #Not sure what this means or how they got it
+				metrics=['accuracy'])
+
+#Time to train the model on the training data
+model.fit(train_images, train_labels, epochs=5)
+
+#Now time to test the model on the test data that we have
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print('Test accuracy: ', test_acc)
+
+def plot_image(i, predictions_array, true_label, img):
+	predictions_array, true_label, img = predictions_array[i], true_label[i], img[i] #I don't understand this line at all
+	plt.grid(False)
 	plt.xticks([])
 	plt.yticks([])
+	
+	plt.imshow(img, cmap=plt.cm.binary)
+	
+	predicted_label = np.argmax(predictions_array)
+	if predicted_label==true_label:
+		color = 'blue'
+	else:
+		color = 'red'
+	
+	plt.xlabel("{} {:2.0f} {}".format(class_names[predicted_label], 100*np.max(predictions_array), class_names[true_label], color=color))
+	
+def plot_value_array(i, predictions_array, true_label):
+	predictions_array, true_label = predictions_array[i], true_label[i]
 	plt.grid(False)
-	plt.imshow(train_images[i], cmap=plt.cm.binary)
-	plt.xlabel(class_names[train_labels[i]])
-plt.show()
+	plt.xticks([])
+	plt.yticks([])
+	
+	thisplot = plt.pie(predictions_array, labels=class_names)
+	predicted_label = np.argmax(predictions_array)
+	
+	#thisplot[predicted_label].set_color('red') Getting a tuple index error here, so just gonna skip the color part of it
+	#thisplot[true_label].set_color('blue')
 
+predictions = model.predict(test_images)
+
+i = 0
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions, test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions, test_labels)
+plt.show()
